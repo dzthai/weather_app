e87387c04f6ffcafb85762a2849b79683869e3f4
David Thai <dzthai@ucsd.edu>
2021-12-20 18:47:30 -0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,10 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body:
+          Container(
+            child: Text('Hassan is fuck'),
+            color: Colors.blue,
         ),
       ),
     );
